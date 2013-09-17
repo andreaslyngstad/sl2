698114ed58e47a7a673faaccc58622de3820d709
Andreas Lyngstad <andreaslyngstad@gmail.com>
2013-09-18 00:13:27 +0200

    deployment error fixing3

--- config/initializers/queue_classic.rb
+++ config/initializers/queue_classic.rb
@@ -1,2 +1,2 @@
-ENV["DATABASE_URL"] = "postgres://squadlink:" + SECRETS_CONFIG["production"][:postgrespassword].to_s + "@162.243.11.153/squadlink_production"
+ENV["DATABASE_URL"] = "postgres://squadlink:" + SECRETS_CONFIG["production"][:postgrespassword].to_s + "@localhost/squadlink_production"
 $email_jobs = QC::Queue.new("email_jobs")
