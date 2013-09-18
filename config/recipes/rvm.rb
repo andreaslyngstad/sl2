b6b2d7dd32f52824b8a09539ea23669639fe01f8
Andreas Lyngstad <andreaslyngstad@gmail.com>
2013-09-18 12:20:24 +0200

    deployment error fixing6

--- config/recipes/rvm.rb
+++ config/recipes/rvm.rb
@@ -1,12 +1,10 @@
-# set_default :ruby_version, "1.9.3-p125"
-# set_default :rbenv_bootstrap, "bootstrap-ubuntu-10-04"
-
-# namespace :rvm do
-#   desc "Install rvm, Ruby, and the Bundler gem"
-#   task :install, roles: :app do
-#     run "\curl -L https://get.rvm.io | bash -s stable --ruby"
-#     run "source /home/deployer/.rvm/scripts/rvm"
-#     run "gem install bundler --no-ri --no-rdoc"
-#   end
-#   after "deploy:install", "rbenv:install"
-# end
+namespace :rvm do
+  desc "Install rvm, Ruby, and the Bundler gem"
+  task :install, roles: :app do
+  	run "#{sudo} apt-get -y install curl git-core"
+    run "\curl -L https://get.rvm.io | bash -s stable --ruby"
+    run "source /home/deployer/.rvm/scripts/rvm"
+    run "gem install bundler --no-ri --no-rdoc"
+  end
+  after "deploy:install", "rbenv:install"
+end
