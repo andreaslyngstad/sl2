d542303c461f2e53e7e7190406f9c9a97ab11359
Andreas Lyngstad <andreaslyngstad@gmail.com>
2013-09-18 10:14:17 +0200

    deployment error fixing5

--- config/initializers/queue_classic.rb
+++ config/initializers/queue_classic.rb
@@ -1,4 +1,5 @@
 ENV["DATABASE_URL"] = "postgres://squadlink:" + SECRETS_CONFIG["production"][:postgrespassword].to_s + "@localhost/squadlink_production"
+puts SECRETS_CONFIG["production"][:postgrespassword]
 require 'queue_classic'
 QC::Conn.connection = ActiveRecord::Base.connection.raw_connection
 $email_jobs = QC::Queue.new("email_jobs")
