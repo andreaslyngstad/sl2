a8bf6c1117e11c8fddedc0c2f343c76162be32a2
Andreas Lyngstad <andreaslyngstad@gmail.com>
2014-09-20 17:05:53 -0400

    Add secret free

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,7 +6,7 @@ class PlansController < ApplicationController
   	else
   		currency = params[:currency] || current_firm.plan.currency
 	end
-    @plans = Plan.where(currency: currency).order("price").where.not(name: "SercretFREE")
+    @plans = Plan.where(currency: currency).order("price").where.not(name: "SecretFREE")
   end
   def cancel
   	authorize! :manage, Firm
